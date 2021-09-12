07d517e64909a81738838bbb09ddf948d294fcaf
evoart-studio <75247931+evoart-studio@users.noreply.github.com>
2021-09-12 16:09:29 +0300

    Update FilterGroup.php

--- app/Models/FilterGroup.php
+++ app/Models/FilterGroup.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Filter;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class FilterGroup extends Model
 {
@@ -14,7 +14,8 @@ class FilterGroup extends Model
         'name'
     ];
 
-    public function filters() {
+    public function filters(): HasMany
+    {
         return $this->hasMany(Filter::class, 'group_id', 'id');
     }
 }
